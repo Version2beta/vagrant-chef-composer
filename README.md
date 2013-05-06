f8f954b964d5805fc2bbb4af76c908cfdccdc1d3
Rob Martin <rob@version2beta.com>
2013-05-06 14:31:43 -0500

    Updated to Vagrant 1.2 and improved docs

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 vagrant-chef-composer
 =====================
 
-A shortcut to a dev environment with PHP Composer on Vagrant provisioned by Chef
+A shortcut to a dev environment with PHP Composer on Vagrant (1.2+) provisioned by Chef.
+
+Composer is a package management system for PHP. You can get more information about it at [getcomposer.org](http://getcomposer.org).
+
+The short form is this:
+
+* Change to the project directory.
+* Edit composer.json
+* Run ```composer install```.
+
+This particular installation comes with the Slim Framework listed in the composer.json. Slim is a PHP microframework inspired by projects like Sinatra and Flask.
--- cookbooks/configure/recipes/default.rb
+++ cookbooks/configure/recipes/default.rb
@@ -48,32 +48,6 @@ cookbook_file "/home/vagrant/.vimrc" do
   mode 00755
 end
 
-gitbag = data_bag_item("git", "ssh_keys")
-ssh_public = gitbag["_default"]["public_key"]
-ssh_private = gitbag["_default"]["private_key"]
-known_hosts = gitbag["_default"]["known_hosts"]
-
-file "/home/vagrant/.ssh/id_rsa.pub" do
-  content ssh_public
-  owner "vagrant"
-  group "vagrant"
-  mode 00600
-end
-
-file "/home/vagrant/.ssh/id_rsa" do
-  content ssh_private
-  owner "vagrant"
-  group "vagrant"
-  mode 00600
-end
-
-file "/home/vagrant/.ssh/known_hosts" do
-  content known_hosts
-  owner "vagrant"
-  group "vagrant"
-  mode 00600
-end
-
 cookbook_file "/home/vagrant/.gitconfig" do
   source "gitconfig"
   owner "vagrant"
